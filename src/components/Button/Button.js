b3c9d0205a2c97ac8758af3db584ab974e69d5dc
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2022-12-21 00:03:00 -0600

    Fix invalid property name

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,8 +3,8 @@ import "./Button.css";
 export default function Button({ text, onClick }) {
   return (
     <>
-      <button onClick={onClick} class="btn-3d">
-        <p class="button__text">{text}</p>
+      <button onClick={onClick} className="btn-3d">
+        <p className="button__text">{text}</p>
       </button>
     </>
   )
