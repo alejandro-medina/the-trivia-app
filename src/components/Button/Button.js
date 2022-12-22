8b452219e9c3ac06012ba83e3bb9f9378617903c
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2022-12-21 18:04:59 -0600

    Add support for disabled prop to button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import "./Button.css";
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, disabled = false }) {
   return (
     <>
-      <button onClick={onClick} className="btn-3d">
+      <button disabled={disabled} onClick={onClick} className="btn-3d">
         <p className="button__text">{text}</p>
       </button>
     </>
