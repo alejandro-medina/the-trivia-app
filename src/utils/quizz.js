a80ae6fc53cb7e48a9b07a1021cb003c458fb58d
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2022-12-21 23:35:11 -0600

    Format the response from the API
    - Add answers indicator for correct or incorrect in same array

--- src/utils/quizz.js
+++ src/utils/quizz.js
@@ -11,8 +11,41 @@ const getTrivia = ({ limit = 10, difficulty = "hard" }) => {
     .then(data => data);
 }
 
+/**
+ * Gets the trivia array from the API and parses to 
+ * get the desired format
+ * 
+ * Then order the answers alphabetical
+ * 
+ * @param {array} trivia Raw trivia array from API
+ */
+const transformTrivia = (trivia) => {
+  return trivia.map(q => {
+    return {
+      question: q.question,
+      answers: [...q.incorrectAnswers.map(a => {
+        return {
+          answer: a,
+          correct: false
+        }
+      }), {
+        answer: q.correctAnswer,
+        correct: true
+      }].sort((a, b) => {
+        if (a.answer < b.answer) return -1;
+        if (a.answer > b.answer) return 1;
+        return 0;
+      }),
+      id: q.id,
+      category: q.category,
+      type: q.type,
+      difficulty: q.difficulty
+    }
+  })
+}
+
 const generateTrivia = async () => {
-  return await getTrivia({});
+  return transformTrivia(await getTrivia({}));
 }
 
 export {
