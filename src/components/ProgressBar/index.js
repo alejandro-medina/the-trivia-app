2d90ca209e0c7db9fc76820620e559417f8db879
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-05 18:49:59 -0600

    Refactor progress bar component

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,10 +1,17 @@
+import styles from './index.module.css';
+
 export default function ProgressBar({ value = 0, max = 0 }) {
+
+  const calcPercent = (value, total) => {
+    return Math.floor(value * 100 / total);
+  }
+
+  const percent = calcPercent(value, max) + "%";
+
   return (
-    <progress
-      style={{
-        width: '100%'
-      }}
-      value={value}
-      max={max} />
+    <div className={styles['progress-bar']}>
+      <div className={styles['progress']} style={{ width: percent }}>
+      </div>
+    </div>
   )
 }
