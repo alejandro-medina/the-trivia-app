951fee1b1b70b8674beff46344fb7d1ab291e94b
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-08 00:50:04 -0600

    Refactor Badge component

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -1,11 +1,12 @@
 import styles from './index.module.css';
 
-export default function Badge({ children, className = '' }) {
+export default function Badge({ children, className }) {
+
+  const { badge, [className]: type } = styles;
+
   return (
-    <>
-      <p className={`${styles.badge} ${className ? styles[className] : ''}`}>
-        {children}
-      </p>
-    </>
+    <p className={`${badge} ${type}`}>
+      {children}
+    </p>
   )
 }
