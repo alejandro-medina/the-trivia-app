331959b7c19c6e7aa3d934ea3469cac28d91d4ef
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-08 00:55:23 -0600

    Refactor components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,8 @@ import "./Button.css";
 
 export default function Button({ text, onClick, disabled = false }) {
   return (
-    <>
-      <button disabled={disabled} onClick={onClick} className="btn-3d">
-        <p className="button__text">{text}</p>
-      </button>
-    </>
+    <button disabled={disabled} onClick={onClick} className="btn-3d">
+      <p className="button__text">{text}</p>
+    </button>
   )
 }
--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,15 +2,12 @@ import styles from './index.module.css';
 
 export default function ProgressBar({ value = 0, max = 0 }) {
 
-  const calcPercent = (value, total) => {
-    return Math.floor(value * 100 / total);
-  }
-
-  const percent = calcPercent(value, max) + "%";
+  const { progress, 'progress-bar': progress_bar } = styles;
+  const percent = Math.floor(value * 100 / max) + "%";
 
   return (
-    <div className={styles['progress-bar']}>
-      <div className={styles['progress']} style={{ width: percent }}>
+    <div className={progress_bar}>
+      <div className={progress} style={{ width: percent }}>
       </div>
     </div>
   )
