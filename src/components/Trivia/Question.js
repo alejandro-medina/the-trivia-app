17381d08a4cad341f980e527ea7dfcec2175124f
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-09 00:30:19 -0600

    Refactor

--- src/components/Trivia/Question.js
+++ src/components/Trivia/Question.js
@@ -2,14 +2,18 @@ import Answer from "./Answer";
 import styles from "./Question.module.css";
 
 export default function Question({ question, onSelectAnswer }) {
+  const { title } = styles;
   return (
     <div>
-      <p className={styles.title}>
+      <p className={title}>
         {question.question}
       </p>
       <div>
         {question.answers.map((answer, idx) => {
-          return <Answer key={`q-${question.id}-a-${idx}`} answerText={answer.answer} selected={answer.selected} onSelectAnswer={() => {onSelectAnswer(idx)}} />
+          return <Answer key={`q-${question.id}-a-${idx}`}
+            answerText={answer.answer}
+            selected={answer.selected}
+            onSelectAnswer={() => { onSelectAnswer(idx) }} />
         })}
       </div>
     </div>
