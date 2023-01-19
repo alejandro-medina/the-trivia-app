297a19b6de03f8067954b9430d0e4a2cc840b34e
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-19 16:36:23 -0600

    Update trivia cover style

--- src/components/TriviaCover.js
+++ src/components/TriviaCover.js
@@ -2,16 +2,14 @@ import Button from "./Button/Button"
 
 export default function Cover({ startQuizz, loading }) {
   return (
-    <div className="app">
-      <h1>Bienvenido al quizz</h1>
-      <p>¡Pon a prueba tus conocimientos y supera cada vez más tu propio record!</p>
-      <br />
-      <Button text="Comenzar quizz" onClick={startQuizz} disabled={loading} />
-      <p>
-        <small>
-          * Por el momento, el quizz solo se encuentra disponible en <b>inglés</b>
-        </small>
-      </p>
+    <div className="cover">
+      <div className="app">
+        <h1 className="cover__title">The Trivia App</h1>
+        <p className="cover__description">
+          Play unlimited times, enjoy!
+        </p>
+        <Button text="Play!" onClick={startQuizz} disabled={loading} />
+      </div>
     </div>
   )
 }
