6bcd17ce6073be2fb6a6f73cad57e7950df70a58
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-19 18:32:52 -0600

    Separate footer into new component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import "./index.module.css";
+
+export default function Footer() {
+  return (
+    <div className="app">
+      <p>
+        Created by
+        <a href="https://github.com/alejandro-medina"
+          target="_blank" rel="noreferrer"> Alejandro Medina
+        </a>
+      </p>
+      <p>Trivias from
+        <a href="https://the-trivia-api.com/"
+          target="_blank" rel="noreferrer"> The Trivia API
+        </a>
+      </p>
+      <a href="https://www.flaticon.com/free-icons/quiz"
+        title="quiz icons">Quiz icons created by Freepik - Flaticon
+      </a>
+    </div>
+
+  )
+}
