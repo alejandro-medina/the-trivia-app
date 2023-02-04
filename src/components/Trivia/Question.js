62f094def4a642ea6af7555edcd7b0906f246a31
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-02-04 17:21:36 -0600

    add keydown listener for selecting question options

--- src/components/Trivia/Question.js
+++ src/components/Trivia/Question.js
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
 import Answer from "./Answer";
 import styles from "./Question.module.css";
 
 export default function Question({ question, onSelectAnswer }) {
   const { title, answers__container } = styles;
+
+  const KeyDownHandler = (e) => {
+    const availableOptions = ["1", "2", "3", "4"];
+    if (availableOptions.includes(e.key)) {
+      e.preventDefault();
+      const idx = e.key - 1;
+      onSelectAnswer(idx);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", KeyDownHandler);
+    return () => {
+      document.removeEventListener("keydown", KeyDownHandler);
+    }
+  }, [question])
+
   return (
     <div>
       <p className={title}>
